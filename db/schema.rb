c9bceaf388ced67a6218b93129c06b1bc879497c
Wellington Perez <wellingtonaperez@gmail.com>
2017-06-06 23:14:42 -0300

    Create user and profile controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604214849) do
+ActiveRecord::Schema.define(version: 20170606233724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,20 @@ ActiveRecord::Schema.define(version: 20170604214849) do
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.boolean  "social_entity"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -49,4 +63,5 @@ ActiveRecord::Schema.define(version: 20170604214849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "profiles", "users"
 end
