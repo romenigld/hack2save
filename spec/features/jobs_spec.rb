581ec6a25180b7374d93c37b94c3e3cc6f63cac1
Raimundo Amorim <rjunior_amorim@hotmail.com>
2017-06-08 10:11:50 -0300

    Tests create invalid attributes

--- spec/features/jobs_spec.rb
+++ spec/features/jobs_spec.rb
@@ -10,17 +10,30 @@ RSpec.describe 'Jobs', type: :feature do
     let(:job) {build :job}
     
     it 'valid attributes' do 
-
       visit new_user_job_path(@user)
 
-      fill_in 'Title', with: job.title
-      fill_in 'Description', with: job.description
-
-      click_button 'Create'
+      job_form(job, 'Create')    
 
       expect(page).to have_content job.title
       expect(page).to have_content job.description
     end
+
+    it 'invalid attributes' do 
+      visit new_user_job_path(@user)
+      new_job = build(:job, title: nil, description: nil)
+
+      job_form(new_job, 'Create')    
+
+      expect(page).to have_content "can't be blank"
+    end
   end
 
+end
+
+def job_form(attribute, button)
+  
+  fill_in 'Title', with: attribute.title
+  fill_in 'Description', with: attribute.description
+
+  click_button button
 end
