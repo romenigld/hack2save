c1d819da8bb6bd2d99041b3f92666e9c04aee4a2
Raimundo Amorim <rjunior_amorim@hotmail.com>
2017-06-08 10:05:28 -0300

    Test create jobs

--- spec/features/jobs_spec.rb
+++ spec/features/jobs_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Jobs', type: :feature do 
-
+  
   before do
     @user = login_user
   end
 
   describe '#create' do
+    let(:job) {build :job}
     
     it 'valid attributes' do 
+
       visit new_user_job_path(@user)
+
+      fill_in 'Title', with: job.title
+      fill_in 'Description', with: job.description
+
+      click_button 'Create'
+
+      expect(page).to have_content job.title
+      expect(page).to have_content job.description
     end
   end
 
