2a15f6e5ec4a223e508bd692c1d53c7e7c4aada1
romenigld <romenigld@gmail.com>
2017-06-08 19:22:05 +0200

    Created and set up factories with FFaker

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,12 +1,13 @@
+require 'ffaker'
+
 FactoryGirl.define do
   factory :profile do
-    name "MyString"
-    last_name "MyString"
-    job_title "MyString"
+    name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    job_title {FFaker::Job.title }
     social_entity false
-    phone_number "MyString"
-    contact_email "MyString"
-    description "MyText"
-    user nil
+    phone_number { FFaker::PhoneNumber.phone_number }
+    contact_email { FFaker::Internet.email }
+    description { FFaker::Book.description }
   end
 end
