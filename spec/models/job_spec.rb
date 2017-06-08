2dbcf31916fb78606b60f9ed09d321fde00d7a53
Raimundo Amorim <rjunior_amorim@hotmail.com>
2017-06-07 22:39:38 -0300

    Test model job user_id

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe Job, type: :model do
     expect(job).to be_valid
   end 
 
+  it 'is not valid without a user' do
+    job = Job.new(user_id: nil)
+    expect(job).to_not be_valid
+  end
+
   it 'is not valid without a title' do
     job = Job.new(title: nil)
     expect(job).to_not be_valid
   end
+
   it 'is not valid without a description' do
     job = Job.new(title: 'title', description: nil)
     expect(job).to_not be_valid
