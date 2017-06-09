28f16cc3dd5e6ce3f31e8d5100d9a04a2ccff786
romenigld <romenigld@gmail.com>
2017-06-10 01:56:20 +0200

    seeds incompleted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ["romenigld", "lfpimenta", "wellingtonp", "velders", "raimundojra", "fulano", "sicrano"].each do |name|
+#   unless User.exists?(name: name)
+#     User.create!(email: "#{name}@hack2save.com", password: "password")
+#   end
+# end
