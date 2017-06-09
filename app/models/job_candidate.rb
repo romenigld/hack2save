41eaa4dbf270f28884b1f331171eef4babc1cf06
Raimundo Amorim <rjunior_amorim@hotmail.com>
2017-06-09 16:31:03 -0300

    Accept candidate

--- app/models/job_candidate.rb
+++ app/models/job_candidate.rb
@@ -1,6 +1,6 @@
 class JobCandidate < ApplicationRecord
   belongs_to :user
   belongs_to :job
-
+  enum status: [:waiting, :accept, :recused]
   validates :presentation_letter, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   
   get 'job_candidates/:job_id/new', to: 'job_candidates#new'
   post 'job_candidates/:job_id', to: 'job_candidates#create', as: 'job_candidates'
+  put 'job_candidates/:id/:status', to: 'job_candidates#update', as: 'job_candidate_status'
 
   root 'page#index'
 
