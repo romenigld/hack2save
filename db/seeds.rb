6f45c09422e5b6b3094f800290718de80d9ff173
romenigld <romenigld@gmail.com>
2017-06-10 15:31:29 +0200

    add felipens user on seeds who is a member of the team 4 that I lost to add.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-["romenigld", "lfpimenta", "wellingtonp", "velders", "raimundojra", "team4"].each do |name|
+["romenigld", "lfpimenta", "felipens", "wellingtonp", "velders", "raimundojra", "team4"].each do |name|
   unless User.exists?(email: '#{name}@hack2save.com')
     User.create!(email: "#{name}@hack2save.com", password: "password")
   end
